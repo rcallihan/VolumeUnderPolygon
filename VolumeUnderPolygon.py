b70a747a8c5097a7a0d6fe14211600892546453a
Ryan Callihan <rcallihan1@gmail.com>
2014-04-02 13:27:35 -0700

    add tbx and tbx functionality

--- VolumeUnderPolygon.py
+++ VolumeUnderPolygon.py
@@ -7,50 +7,59 @@ arcpy.CheckOutExtension("3D")
 arcpy.CheckOutExtension("spatial")
 
 #Input Parameters 
-OutputLocation = r"C:\Users\IEUser\Desktop\playafiles\VolTest"
-Elevation_Raster = "C:\Users\IEUser\Desktop\playafiles\lidar_huc12_extract1.img"
-Gulley_Boundary_Polygon = "C:\Users\IEUser\Desktop\playafiles\VolTest\single_playa2.shp"
+# OutputLocation = r"C:\Users\IEUser\Desktop\playafiles\VolTest"
+# Elevation_Raster = "C:\Users\IEUser\Desktop\playafiles\lidar_huc12_extract1.img"
+# Input_Polygon_Shp = "C:\Users\IEUser\Desktop\playafiles\VolTest\single_playa2.shp"
 
-# Set Geoprocessing environments
+#in put from Arc Toolbox. Comment out below and uncomment above if wanting a stand-a-lone script. 
+OutputLocation = arcpy.GetParameterAsText(0)
+Elevation_Raster = arcpy.GetParameterAsText(1)
+Input_Polygon_Shp = arcpy.GetParameterAsText(2)
+
+#set Geoprocessing environments
 arcpy.env.snapRaster = Elevation_Raster
 arcpy.env.cellSize = Elevation_Raster
 arcpy.env.overwriteOutput = True
+arcpy.env.workspace = OutputLocation
 
 #get env sell size, spatial reference
 sr = arcpy.Describe(Elevation_Raster).spatialReference
 CellSizeResult = arcpy.GetRasterProperties_management(Elevation_Raster, "CELLSIZEX")
 Cellsize = CellSizeResult.getOutput(0)
 
-#set workspace
-arcpy.env.workspace = OutputLocation
-
 try:
-	arcpy.DeleteField_management(Gulley_Boundary_Polygon, "Volume")
-	arcpy.FeatureToRaster_conversion(Gulley_Boundary_Polygon, "FID", "Gully_Mask_Raster.img", Cellsize)
+	arcpy.DeleteField_management(Input_Polygon_Shp, "Volume")
+	arcpy.FeatureToRaster_conversion(Input_Polygon_Shp, "FID", "Poly_Mask", Cellsize)
 
 	#Convert Poly verticies to points, extract raster value to poly points, convert poly to raster
-	arcpy.FeatureVerticesToPoints_management(Gulley_Boundary_Polygon, "Gulley_Boundary_Points.shp", "ALL")
-	ExtractValuesToPoints("Gulley_Boundary_Points.shp", Elevation_Raster, "Gully_Points_with_Elevation.shp", "NONE", "VALUE_ONLY")
+	arcpy.FeatureVerticesToPoints_management(Input_Polygon_Shp, "Poly_Boundary_Points.shp", "ALL")
+	ExtractValuesToPoints("Poly_Boundary_Points.shp", Elevation_Raster, "Poly_Points_with_Elevation.shp", "NONE", "VALUE_ONLY")
 
-	output_tin = OutputLocation + "/Poly_Boundary_Tin"
-	arcpy.CreateTin_3d(output_tin, sr, "Gully_Points_with_Elevation.shp RASTERVALU masspoints", "DELAUNAY")
+	#converts raster with elevation points to a TIN. 
+	#output_tin = OutputLocation + "/Poly_Boundary_Tin"
+	arcpy.CreateTin_3d("Poly_Boundary_Tin", sr, "Poly_Points_with_Elevation.shp RASTERVALU masspoints", "DELAUNAY")
 
 	#Tin to raster
 	TinRastCellSize = "CELLSIZE " + Cellsize
-	arcpy.TinRaster_3d(output_tin, "cap_raster", "FLOAT", "", TinRastCellSize, "")
+	arcpy.TinRaster_3d("Poly_Boundary_Tin" "cap_raster", "FLOAT", "", TinRastCellSize, "")
 
-	#sets elevation pixels outside of polygon of interest to null.  
-	outElevationraster = SetNull((IsNull("Gully_Mask_Raster.img")), Elevation_Raster)
-	#Ignore pixels with negative depth values
+	#sets elevation pixels outside of polygon of interest to null. Ignore pixels with negative depth values 
+	outElevationraster = SetNull((IsNull("Poly_Mask")), Elevation_Raster)
 	outDepthraster = Con("cap_raster" > outElevationraster, "cap_raster" - outElevationraster)
 	
 	#calculates volume above raster, adds output to the poly shp
 	arcpy.SurfaceVolume_3d(outDepthraster, '', 'ABOVE')
 	result = arcpy.GetMessages()
 	volume = float(re.findall(r'Volume= *([\d\.]+)', result)[0])
-	arcpy.AddField_management(Gulley_Boundary_Polygon, "Volume", "FLOAT", "15", "4")
-	arcpy.CalculateField_management(Gulley_Boundary_Polygon, "Volume", float(volume))
-	print volume
+	arcpy.AddField_management(Input_Polygon_Shp, "Volume", "FLOAT", "15", "4")
+	arcpy.CalculateField_management(Input_Polygon_Shp, "Volume", float(volume))
+	
+	#cleaning up intermediate files
+	for filename in ["Gulley_Boundary_Points.shp", "Gully_Mask_Raster.img", "Gully_Points_with_Elevation.shp", outElevationraster, outDepthraster, "cap_raster", output_tin]:
+		if arcpy.Exists(filename):
+			arcpy.Delete_management(filename)
+	arcpy.AddMessage("Done cleaning intermediate files.")
+
 
 except arcpy.ExecuteError:
 	print arcpy.GetMessages()
@@ -62,3 +71,6 @@ except:
 	msgs = 'ArcPy ERRORS:\n {0}\n' .format(arcpy.GetMessages(2))
 	arcpy.AddError(pymsg)
 	arcpy.AddError(msgs)
+
+arcpy.CheckInExtension("3D")
+arcpy.CheckInExtension("spatial")
